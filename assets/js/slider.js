c8255cf4f4b5340cf0491631b6e2f975c3c7cf9d
Tobia Prezioso <tobia_prezioso@proseller.ch>
2020-10-16 13:51:45 +0200

    improved slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -5,108 +5,102 @@
 const sliders = document.querySelectorAll('.slider');
 
 for (let i = 0; i < sliders.length; i++) {
-    let slider = sliders[i];
-    let sliderControls = slider.querySelectorAll('.slider-control');
-    let sliderItems = slider.querySelectorAll('.slider-item');
-    let autoslideBtn = slider.querySelector('.autoslide');
-    let sliderId = `slider-${i}`;
-    let interval;
+    let sliderObj = {
+        slider: sliders[i],
+        sliderId: `slider-${i}`,
 
-    addIdToSlider(slider, sliderId);
-    slider.setAttribute('data-slider-play', true);
-    updateAutoplay(slider, sliderItems, interval);
-    addSliderIdToControl(autoslideBtn, sliderId)
+        init() {
+            this.sliderControls = this.slider.querySelectorAll('.slider-control');
+            this.sliderItems = this.slider.querySelectorAll('.slider-item');
+            this.autoslideBtn = this.slider.querySelector('.autoslide');
+            this.slider.id = this.sliderId;
+            this.slider.setAttribute('data-slider-play', true);
+            this.initSliderControl();
+            this.updateAutoplay();
+        },
+        initSliderControl() {
+            for (let j = 0; j < this.sliderControls.length; j++) {
+                let control = this.sliderControls[j];
+                control.href = `#${this.sliderId}`;
+                this.addEventListenerToControl(control);
+            }
+            this.autoslideBtn.href = `#${this.sliderId}`;
+            this.addEventListenerToAutoslideBtn();
+        },
+        addEventListenerToControl(control) {
+            control.addEventListener('click', e => {
+                if (control.className.includes('next')) {
+                    this.slideNext();
+                }
+                if (control.className.includes('prev')) {
+                    this.slidePrev();
+                }
+            })
+        },
+        addEventListenerToAutoslideBtn() {
+            this.autoslideBtn.addEventListener('click', e => {
+                if (this.slider.getAttribute('data-slider-play') === "true") {
+                    e.target.classList.replace('fa-pause-circle', 'fa-play-circle');
+                    this.slider.setAttribute('data-slider-play', false);
+                    this.updateAutoplay();
+                } else {
+                    e.target.classList.replace('fa-play-circle', 'fa-pause-circle');
+                    this.slider.setAttribute('data-slider-play', true);
+                    this.updateAutoplay();
+                }
+            })
+        },
+        slideNext() {
+            let activeItem = this.slider.getAttribute('data-active-slide');
 
-    for (let j = 0; j < sliderControls.length; j++) {
-        let control = sliderControls[j];
-        addSliderIdToControl(control, sliderId);
-        addEventListenerToControl(slider, control, sliderItems);
-    }
+            if (activeItem < this.sliderItems.length) {
+                activeItem++;
+                this.updateActiveSlideData(activeItem)
+                this.sliderItems.forEach(item => {
+                    item.style.transform = `translateX(-${(activeItem - 1) * 100}%)`;
+                })
+            } else {
+                activeItem = 1;
+                this.updateActiveSlideData(activeItem)
+                this.sliderItems.forEach(item => {
+                    item.style.transform = `translateX(${0}%)`;
+                })
+            }
 
-    autoslideBtn.addEventListener('click', e => {
-        if (slider.getAttribute('data-slider-play') === "true") {
-            e.target.classList.replace('fa-pause-circle', 'fa-play-circle');
-            slider.setAttribute('data-slider-play', false);
-            updateAutoplay(slider, sliderItems, interval);
-        } else {
-            e.target.classList.replace('fa-play-circle', 'fa-pause-circle');
-            slider.setAttribute('data-slider-play', true);
-            updateAutoplay(slider, sliderItems, interval);
+        },
+        slidePrev() {
+            let activeItem = this.slider.getAttribute('data-active-slide');
+
+            if (activeItem > 1) {
+                activeItem--;
+                this.updateActiveSlideData(activeItem)
+                this.sliderItems.forEach(item => {
+                    item.style.transform = `translateX(-${(activeItem - 1) * 100}%)`;
+                })
+            } else {
+                activeItem = this.sliderItems.length;
+                this.updateActiveSlideData(activeItem)
+                this.sliderItems.forEach(item => {
+                    item.style.transform = `translateX(-${(this.sliderItems.length - 1) * 100}%)`;
+                })
+            }
+
+        },
+        updateAutoplay() {
+
+            if (this.slider.getAttribute('data-slider-play') === "true") {
+                this.interval = setInterval(e => {
+                    this.slideNext();
+                }, 5000)
+            } else {
+                clearInterval(this.interval);
+            }
+        },
+        updateActiveSlideData(index) {
+            this.slider.setAttribute('data-active-slide', index)
         }
-    })
-}
-
-function addIdToSlider(slider, sliderId) {
-    slider.id = sliderId;
-}
-
-function addSliderIdToControl(control, sliderId) {
-    control.href = `#${sliderId}`;
-}
-
-function addEventListenerToControl(slider, control, sliderItems) {
-
-    control.addEventListener('click', e => {
-        if (control.className.includes('next')) {
-            slideNext(slider, sliderItems);
-        }
-        if (control.className.includes('prev')) {
-            slidePrev(slider, sliderItems);
-        }
-
-    })
-
-}
-
-function slideNext(slider, sliderItems) {
-    let activeItem = slider.getAttribute('data-active-slide');
-
-    if (activeItem < sliderItems.length) {
-        activeItem++;
-        updateActiveSlideData(slider, activeItem)
-        sliderItems.forEach(item => {
-            item.style.transform = `translateX(-${(activeItem - 1) * 100}%)`;
-        })
-    } else {
-        activeItem = 1;
-        updateActiveSlideData(slider, activeItem)
-        sliderItems.forEach(item => {
-            item.style.transform = `translateX(${0}%)`;
-        })
     }
 
-}
+    sliderObj.init();
 
-function slidePrev(slider, sliderItems) {
-    let activeItem = slider.getAttribute('data-active-slide');
-
-    if (activeItem > 1) {
-        activeItem--;
-        updateActiveSlideData(slider, activeItem)
-        sliderItems.forEach(item => {
-            item.style.transform = `translateX(-${(activeItem - 1) * 100}%)`;
-        })
-    } else {
-        activeItem = sliderItems.length;
-        updateActiveSlideData(slider, activeItem)
-        sliderItems.forEach(item => {
-            item.style.transform = `translateX(-${(sliderItems.length - 1) * 100}%)`;
-        })
-    }
-
-}
-
-function updateAutoplay(slider, sliderItems, interval) {
-
-    if (slider.getAttribute('data-slider-play') === "true") {
-        interval = setInterval(function (){
-            slideNext(slider, sliderItems);
-        }, 3500)
-    } else {
-        clearInterval(interval);
-    }
-}
-
-function updateActiveSlideData(slider, number) {
-    slider.setAttribute('data-active-slide', number)
 }
